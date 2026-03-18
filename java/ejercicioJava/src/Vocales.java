a1ecd50c787b415b0126799ebd39675d531431c9
rodrigomasek <alumno27.masek.urquizu.rodrigo@ipm.edu.ar>
2026-03-18 09:04:37 -0300

    empanadas

---
+++ java/ejercicioJava/src/Vocales.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Vocales {
+    public static void main(String[] args) {
+        String texto = "Ayer me compre muñecos de la marca ‘ToyCo’ por internet.";
+        int i;
+        int vocales = 0;
+
+        for(i = 0; texto.length() > i; i++)
+        {
+            if(texto.charAt(i) == 'a' || texto.charAt(i) == 'u' || texto.charAt(i) == 'o' || texto.charAt(i) == 'i' || texto.charAt(i) == 'e')
+            {
+                vocales++;
+            }
+        }
+        System.out.println(vocales);
+    }
+}
---
+++ java/ejercicioJava/src/diaLaboral.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class diaLaboral {
+    public static void main(String[] args) {
+        String[] dias = {"sabado","domingo"};
+        Scanner scanner = new Scanner(System.in);
+        String dia;
+        System.out.print("Ingrese un dia de la semana xd: ");
+        dia = scanner.nextLine();
+        if(dia.equals(dias[0]) || dia.equals(dias[1]))
+        {
+            System.out.println("Dia inhabil");
+        }
+        else
+        {
+            System.out.println("Dia habil");
+        }
+    }
+}
