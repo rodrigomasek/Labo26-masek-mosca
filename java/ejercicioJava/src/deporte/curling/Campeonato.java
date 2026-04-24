859a7e38bc22e9d15d8e69f1ed07c1f342f7a002
masekmosca <alumno27.masek.urquizu.rodrigo@ipm.edu.ar>
2026-04-24 16:19:46 -0300

    falta

---
+++ java/ejercicioJava/src/deporte/curling/Campeonato.java
@@ -0,0 +1,25 @@
+package deporte.curling;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+
+public class Campeonato {
+    private ArrayList <Partido> partidos;
+
+    public Campeonato(ArrayList<Partido> partidos) {
+        this.partidos = partidos;
+    }
+
+    public ArrayList<Partido> getPartidos() {
+        return partidos;
+    }
+
+    public void setPartidos(ArrayList<Partido> partidos) {
+        this.partidos = partidos;
+    }
+    public void partidoPosible()
+    {
+
+    }
+
+}
--- java/ejercicioJava/src/deporte/curling/Partido.java
+++ java/ejercicioJava/src/deporte/curling/Partido.java
@@ -1,5 +1,57 @@
 package deporte.curling;
 
+import java.time.LocalDate;
+import java.util.ArrayList;
+
 public class Partido {
+
+    private Equipos equipo1;
+    private Equipos equipo2;
+    private LocalDate fecha;
+    private String turno;
+
+    public Equipos getEquipo1() {
+        return equipo1;
+    }
+
+    public Partido(Equipos equipo1, Equipos equipo2, LocalDate fecha, String turno) {
+        this.equipo1 = equipo1;
+        this.equipo2 = equipo2;
+        this.fecha = fecha;
+        this.turno = turno;
+    }
+
+    public void setEquipo1(Equipos equipo1) {
+        this.equipo1 = equipo1;
+    }
+
+    public Equipos getEquipo2() {
+        return equipo2;
+    }
+
+    public void setEquipo2(Equipos equipo2) {
+        this.equipo2 = equipo2;
+    }
+
+    public LocalDate getFecha() {
+        return fecha;
+    }
+
+    public void setFecha(LocalDate fecha) {
+        this.fecha = fecha;
+    }
+
+    public String getTurno() {
+        return turno;
+    }
+
+    public void setTurno(String turno) {
+        this.turno = turno;
+    }
+
+    public boolean validar(){
+        return (this.equipo1 == this.equipo2);
+    }
+
 
 }
